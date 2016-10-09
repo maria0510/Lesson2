4b70885437c4db357742ca7323090fcfb733feea
maria0510 <mariadubil05@gmail.com>
2016-10-10 00:14:00 +0300

    1 commit

---
+++ Hello/src/some/programm/Hello.java
@@ -0,0 +1,9 @@
+package some.programm;
+
+class HelloStudent {
+    public static void main(String[] args) {
+        // Display "Hello World!"
+        System.out.println("Hello Student!");
+    }
+	}
+	
